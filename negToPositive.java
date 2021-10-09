141d3ea5bc0b81fefde0d666c881326489965d4d
Jisha <90095634+Jishanithin@users.noreply.github.com>
2021-10-09 18:41:55 +0530

    Update negToPositive.java

--- negToPositive.java
+++ negToPositive.java
@@ -6,9 +6,9 @@ public class negToPositive {
 	public static void main(String[] args) {
 		int number = -40;
 		
-		for(number=-40; number==-40; number++) {
-			System.out.println("the number -40 is converted to 40");
-		}
+		if(number<0) {
+			number = number * -1;
+			System.out.println(number);
 	}
 
 }
